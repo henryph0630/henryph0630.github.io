cb09028eb8dc7b07d899698599f6056768ce884a
henryph0630 <henryph0630@outlook.com>
2025-02-23 18:19:29 -0500

    Homepage with Assignment 7

--- csce242/assignments/assignment06/script.js
+++ csce242/assignments/assignment06/script.js
@@ -1,5 +1,43 @@
     const drawStairBtn = document.getElementById("draw-stairs");
+    const stairs = document.getElementById("stairs-container");
 
     drawStairBtn.onclick = () => {
-        
+        stairs.classList.add("railing");
+        for (let i = 0; i < 10; i++){
+            const hr = document.createElement("hr");
+            stairs.append(hr);
+        }
+        stairs.classList.remove("hidden");
+    }
+
+    const climbStairsBtn = document.getElementById("climb-stairs");
+    const stickFigure = document.querySelector("img");
+
+    let count = 0;
+    let intervalClimb;
+
+    const Climb = () => {
+        ++count;
+
+
+        const stickFigureSource = stickFigure.src;
+        if(stickFigureSource.includes("left")){
+            stickFigure.src = "images/right.png";
+        }
+        else{
+            stickFigure.src = "images/left.png";
+        }
+
+        if(count === 12) {
+            clearInterval(intervalClimb);
+        }
+
+        const elevation = Number.parseInt(getComputedStyle(stickFigure).bottom);
+        stickFigure.style.bottom = `${elevation + 32}px`;   
+
+
+    }
+
+    climbStairsBtn.onclick = () => {
+        intervalClimb = setInterval(Climb, 1000);
     }
